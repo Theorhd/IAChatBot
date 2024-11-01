ab91adb1b5c94ba35396c5cd9707396deecfaa6c
Theo <theo.richard8654@gmail.com>
2024-11-01 13:59:45 +0100

    fix

--- myIA/s2t.py
+++ myIA/s2t.py
@@ -12,13 +12,19 @@ def speech_to_text():
         try:
             text = recognizer.recognize_google(audio, language="fr-FR")
             print("Vous avez dit : " + text)
+            return text
         except sr.UnknownValueError:
-            print("Impossible de comprendre l'audio")
+            return None
+        except sr.RequestError as e:
+            return None
         except sr.RequestError as e:
             print("Erreur avec le service Google Speech Recognition ; {0}".format(e))
 
 def liveConv():
     while True:
         text = speech_to_text()
-        if (text):
+        if text is None:
+            print("Je n'ai pas compris ce que vous avez dit. Pouvez-vous répéter ?")
+            continue
+        else:
             chat_t2s(text)
